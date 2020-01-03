1b0c56552e907d64eca07679f1570ee763c0dca6
zhangsw <zhangsenrose@163.com>
2020-01-03 14:29:29 +0800

    集成logback.xml日志

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ssm-scaffold1
+整体架构就是普通的三层 dao service controller
+src/mian/resources 目录下包含SSM框架需要的配置文件，
+spring目录：
+        包括applicationContext.xml,spring-mvc.xml,spring-datesource.xml配置文件，
+        以后可以拓展spring-task.xml,cache.xml等配置
+mybatis目录：
+        该目录包含mybatis的配置文件 mybatis-config.xml 
+        mapper文件夹下存放的是mapper.xml文件，便于管理
+
+webapp文件夹存在说明是一个web项目
+    WEB-INF目录：
+        jsp文件夹，存放jsp文件
+        static 存放静态资源文件，js, css, images, 等...
--- src/main/java/com/ash/cn/controller/UserController.java
+++ src/main/java/com/ash/cn/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ash.cn.controller;
 
 import com.ash.cn.pojo.SysUser;
 import com.ash.cn.service.IUserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +13,7 @@ import javax.annotation.Resource;
 
 @Controller
 public class UserController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
 
     @Resource
@@ -20,6 +23,8 @@ public class UserController {
     @RequestMapping(method = RequestMethod.GET, value = "/getUser")
     @ResponseBody
     public SysUser getUser(Long id) {
+        log.info("进入控制器端....");
+        log.info("参数为：{}", id);
         return  userService.getUserByUserId(id);
     }
 
